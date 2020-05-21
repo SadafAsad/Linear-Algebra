8b7586736ab589706c3d0b374b0191723acf1088
sadaf <sadaf98x@gmail.com>
2020-05-21 21:15:24 +0430

    modified -- decomposition fucntion added

--- permutationMatrixSquareRoot.py
+++ permutationMatrixSquareRoot.py
@@ -9,23 +9,28 @@ def zeroMatrix(n):
         matrix.append(row)
     return matrix
 
-def createCycleList(n, data_str):
+def decomposition(n, data_str):
     cycles_list = list()
     for r in range(n):
         cycle = list()
         x = int(data_str[r])
-        while x!=r:
+        if x!=-1:
+            data_str[r] = -1
             cycle.append(x)
-            x = int(data_str[x])
-        cycle.append(x)
-        cycles_list.append(cycle)
+            while x!=r:
+                tmp = int(data_str[x])
+                data_str[x] = -1
+                x = tmp
+                cycle.append(x)
+            cycles_list.append(cycle)
+    return cycles_list
 
-    for cycle in cycles_list:
-        cycle.sort()
+    # for cycle in cycles_list:
+    #     cycle.sort()
 
-    cycles_list.sort()
-    new_cycles_list = list(cycles_list for cycles_list,_ in itertools.groupby(cycles_list))
-    return new_cycles_list
+    # cycles_list.sort()
+    # new_cycles_list = list(cycles_list for cycles_list,_ in itertools.groupby(cycles_list))
+    # return new_cycles_list
 
 def readDataFromFile(file_name):
     file = open(file_name, "r")
@@ -36,7 +41,7 @@ def readDataFromFile(file_name):
         index = (2*(i+1))-1
         n = int(file_lines[index])
         one_s = file_lines[index+1].split()
-        cycle_list = createCycleList(n, one_s)
+        cycle_list = decomposition(n, one_s)
         samples.append(cycle_list)
     return samples
 
@@ -48,18 +53,21 @@ def hasSqr(cycle_list):
             even_num+=1
         else:
             odd_num+=1
-            
-    if even_num%2==0:
-        if odd_num==0 or odd_num%2==1:
-            return 1
+
     return 0
 
 samples = readDataFromFile("data.in")
+print(samples[0])
+print(samples[1])
 print(samples[2])
+print(samples[3])
+print(samples[4])
 print(samples[5])
+print(samples[6])
+print(samples[7])
 print(samples[8])
-counter = 1
-for sample in samples:
-    if not hasSqr(sample):
-        print("Sample " + str(counter) + " impossible")
-    counter+=1
+# counter = 1
+# for sample in samples:
+#     if not hasSqr(sample):
+#         print("Sample " + str(counter) + " impossible")
+#     counter+=1
