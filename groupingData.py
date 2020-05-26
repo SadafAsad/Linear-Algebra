9f06a33a3c1a10646b1b37c779adc1070f02350f
sadaf <sadaf98x@gmail.com>
2020-05-26 16:33:08 +0430

    cosine similarity function added

--- groupingData.py
+++ groupingData.py
@@ -27,5 +27,23 @@ def norm(point):
     k2 = int(point[3])**2
     return math.sqrt( x2+y2+z2+k2 )
 
+def dotProduct(point1, point2):
+    x = int(point1[0])*int(point2[0])
+    y = int(point1[1])*int(point2[1])
+    z = int(point1[2])*int(point2[2])
+    k = int(point1[3])*int(point2[3])
+    return x+y+z+k
+
+def cosineSimilarity(point, points):
+    cosine_similarities = list()
+    for each_point in points:
+        dot = dotProduct(point, each_point)
+        norm1 = norm(point)
+        norm2 = norm(each_point)
+        cosine_similarities.append(dot/(norm1*norm2))
+    return cosine_similarities
+
+
+
 
 points = readDataFile('dataset.csv')
