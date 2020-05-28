5ce4ad8a28973fdfc369a32f2c010ac9c2cfb609
sadaf <sadaf98x@gmail.com>
2020-05-28 20:52:40 +0430

    #1

--- euclideanDistance.py
+++ euclideanDistance.py
@@ -21,6 +21,66 @@ def euclideanDistance(point, points):
         distances.append(distance)
     return distances
 
+def kMeans(points):
+    p_num = len(points)
+
+    i1 = random.randint(0, p_num-1)
+    i2 = random.randint(0, p_num-1)
+    while i1==i2:
+        i2 = random.randint(0, p_num-1)
+    center1 = points[i1]
+    center2 = points[i2]
+
+    while(True):
+        center1_euclidean_distances = euclideanDistance(center1, points)
+        center2_euclidean_distances = euclideanDistance(center2, points)
+        cluster1 = list()
+        cluster2 = list()
+        center_avg1 = [0,0,0,0]
+        center_avg2 = [0,0,0,0]
+        for i in range(p_num):
+            if center1_euclidean_distances[i]<=center2_euclidean_distances[i]:
+                cluster1.append(points[i])
+                center_avg1[0]+=float(points[i][0])
+                center_avg1[1]+=float(points[i][1])   
+                center_avg1[2]+=float(points[i][2])
+                center_avg1[3]+=float(points[i][3])
+                center_avg1[0]/=2
+                center_avg1[1]/=2
+                center_avg1[2]/=2
+                center_avg1[3]/=2
+            else:
+                cluster2.append(points[i])
+                center_avg2[0]+=float(points[i][0])
+                center_avg2[1]+=float(points[i][1])
+                center_avg2[2]+=float(points[i][2])
+                center_avg2[3]+=float(points[i][3])
+                center_avg2[0]/=2
+                center_avg2[1]/=2
+                center_avg2[2]/=2
+                center_avg2[3]/=2
+
+        if center1==center_avg1 and center2==center_avg2:
+            break
+        center1 = center_avg1
+        center2 = center_avg2
+
+    return (cluster1, cluster2)
+
+
+def d(cluster1, cluster2):
+    flag = 0
+    for p in cluster1:
+        distances_from_p = euclideanDistance(p, cluster2)
+        if flag:
+            local_min = min(distances_from_p)
+            if local_min<=global_min:
+                global_min = local_min
+        else:
+            global_min = min(distances_from_p)
+            flag = 1
+    return global_min
+
 def separating(d, points):
     group1 = list()
     group2 = list()
@@ -42,62 +102,6 @@ def separating(d, points):
         points.pop(0)
     return (group1, group2)
 
-def d(group1, group2):
-    flag = 0
-    for p in group1:
-        distances_from_p = euclideanDistance(p, group2)
-        if flag:
-            local_min = min(distances_from_p)
-            if local_min<=global_min:
-                global_min = local_min
-        else:
-            global_min = min(distances_from_p)
-            flag = 1
-
-# points = readDataFile('dataset.csv')
-# p_num = len(points)
-
-# i1 = random.randint(0, p_num-1)
-# i2 = random.randint(0, p_num-1)
-# while i1==i2:
-#     i2 = random.randint(0, p_num-1)
-# center1 = points[i1]
-# center2 = points[i2]
-
-# while(True):
-#     center1_euclidean_distances = euclideanDistance(center1, points)
-#     center2_euclidean_distances = euclideanDistance(center2, points)
-#     cluster1 = list()
-#     cluster2 = list()
-#     center_avg1 = [0,0,0,0]
-#     center_avg2 = [0,0,0,0]
-#     # d = euclideanDistance(center1, [center2])
-#     for i in range(p_num):
-#         if center1_euclidean_distances[i]<=center2_euclidean_distances[i]:
-#             cluster1.append(points[i])
-#             center_avg1[0]+=float(points[i][0])
-#             center_avg1[1]+=float(points[i][1])
-#             center_avg1[2]+=float(points[i][2])
-#             center_avg1[3]+=float(points[i][3])
-#             center_avg1[0]/=2
-#             center_avg1[1]/=2
-#             center_avg1[2]/=2
-#             center_avg1[3]/=2
-#         else:
-#             cluster2.append(points[i])
-#             center_avg2[0]+=float(points[i][0])
-#             center_avg2[1]+=float(points[i][1])
-#             center_avg2[2]+=float(points[i][2])
-#             center_avg2[3]+=float(points[i][3])
-#             center_avg2[0]/=2
-#             center_avg2[1]/=2
-#             center_avg2[2]/=2
-#             center_avg2[3]/=2
-
-#     if center1==center_avg1 and center2==center_avg2:
-#         break
-#     center1 = center_avg1
-#     center2 = center_avg2
-
-# print(len(cluster1))
-# print(len(cluster2))
+points = readDataFile('database.csv')
+cluster1, cluster2 = kMeans(points)
+print(d(cluster1, cluster2))
