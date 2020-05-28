4eae65f960918a80aa8a520a764703837190392d
sadaf <sadaf98x@gmail.com>
2020-05-28 20:04:36 +0430

    separating function added/tested

--- euclideanDistance.py
+++ euclideanDistance.py
@@ -1,5 +1,6 @@
 from csv import reader
 import math
+import random
 
 def readDataFile(file_name):
     points = list()
@@ -20,44 +21,82 @@ def euclideanDistance(point, points):
         distances.append(distance)
     return distances
 
-points = readDataFile('dataset.csv')
-p_num = len(points)
-center1 = points[0]
-center2 = points[1]
-
-while(True):
-    center1_euclidean_distances = euclideanDistance(center1, points)
-    center2_euclidean_distances = euclideanDistance(center2, points)
-    cluster1 = list()
-    cluster2 = list()
-    center_avg1 = [0,0,0,0]
-    center_avg2 = [0,0,0,0]
-    for i in range(p_num):
-        if center1_euclidean_distances[i]<=center2_euclidean_distances[i]:
-            cluster1.append(points[i])
-            center_avg1[0]+=float(points[i][0])
-            center_avg1[1]+=float(points[i][1])
-            center_avg1[2]+=float(points[i][2])
-            center_avg1[3]+=float(points[i][3])
-            center_avg1[0]/=2
-            center_avg1[1]/=2
-            center_avg1[2]/=2
-            center_avg1[3]/=2
+def separating(d, points):
+    group1 = list()
+    group2 = list()
+    while len(points)!=0:
+        point = points[0]
+        distances = euclideanDistance(point, points)
+        i = len(points)-1
+        flag = 0
+        while i>0:
+            if distances[i]<=d:
+                flag = 1
+                group1.append(points[i])
+                points.pop(i)
+            i-=1
+        if flag:
+            group1.append(points[0])
         else:
-            cluster2.append(points[i])
-            center_avg2[0]+=float(points[i][0])
-            center_avg2[1]+=float(points[i][1])
-            center_avg2[2]+=float(points[i][2])
-            center_avg2[3]+=float(points[i][3])
-            center_avg2[0]/=2
-            center_avg2[1]/=2
-            center_avg2[2]/=2
-            center_avg2[3]/=2
+            group2.append(points[0])
+        points.pop(0)
+    return (group1, group2)
 
-    if center1==center_avg1 and center2==center_avg2:
-        break
-    center1 = center_avg1
-    center2 = center_avg2
+def maxDistance(points):
+    p_len = len(points)
+    for i in range(p_len//2):
+        distances_from_i = euclideanDistance(points[i], points)
+        max_d = 0
+        for d in distances_from_i:
+            if d>=max_d:
+                max_d = d
+    return d
 
-print(len(cluster1))
-print(len(cluster2))
+
+# points = readDataFile('dataset.csv')
+# p_num = len(points)
+
+# i1 = random.randint(0, p_num-1)
+# i2 = random.randint(0, p_num-1)
+# while i1==i2:
+#     i2 = random.randint(0, p_num-1)
+# center1 = points[i1]
+# center2 = points[i2]
+
+# while(True):
+#     center1_euclidean_distances = euclideanDistance(center1, points)
+#     center2_euclidean_distances = euclideanDistance(center2, points)
+#     cluster1 = list()
+#     cluster2 = list()
+#     center_avg1 = [0,0,0,0]
+#     center_avg2 = [0,0,0,0]
+#     # d = euclideanDistance(center1, [center2])
+#     for i in range(p_num):
+#         if center1_euclidean_distances[i]<=center2_euclidean_distances[i]:
+#             cluster1.append(points[i])
+#             center_avg1[0]+=float(points[i][0])
+#             center_avg1[1]+=float(points[i][1])
+#             center_avg1[2]+=float(points[i][2])
+#             center_avg1[3]+=float(points[i][3])
+#             center_avg1[0]/=2
+#             center_avg1[1]/=2
+#             center_avg1[2]/=2
+#             center_avg1[3]/=2
+#         else:
+#             cluster2.append(points[i])
+#             center_avg2[0]+=float(points[i][0])
+#             center_avg2[1]+=float(points[i][1])
+#             center_avg2[2]+=float(points[i][2])
+#             center_avg2[3]+=float(points[i][3])
+#             center_avg2[0]/=2
+#             center_avg2[1]/=2
+#             center_avg2[2]/=2
+#             center_avg2[3]/=2
+
+#     if center1==center_avg1 and center2==center_avg2:
+#         break
+#     center1 = center_avg1
+#     center2 = center_avg2
+
+# print(len(cluster1))
+# print(len(cluster2))
