abc94e01bc0286253f6abe209f72229fc16f586e
sadaf <sadaf98x@gmail.com>
2020-05-29 13:10:15 +0430

    q2 #3

--- cosineSimilarity.py
+++ cosineSimilarity.py
@@ -33,32 +33,55 @@ def cosineSimilarity(point, points):
         cosine_similarities.append(dot/(norm1*norm2))
     return cosine_similarities
 
-def difference(point, points):
-    differences = list()
-    for each_point in points:
-        x = abs(point-each_point)
-        differences.append(x)
-    return differences
+def findStartDistance(points):
+    distances = list()
+    for point in points:
+        d = math.sqrt(float(point[0])**2+float(point[1])**2+float(point[2])**2+float(point[3])**2)
+        distances.append(d)
+    return distances
+
+def tmpCluster(cluster):
+    tmp = list()
+    for i in cluster:
+        tmp.append(i)
+    return tmp
 
 def kMeans(points):
     p_num = len(points)
 
-    i1 = random.randint(0, p_num-1)
-    i2 = random.randint(0, p_num-1)
-    while i1==i2:
-        i2 = random.randint(0, p_num-1)
-    center1 = points[i1]
-    center2 = points[i2]
+    start_d = findStartDistance(points)
+    max_d = 0
+    i_max = 0
+    for i in range(p_num):
+        if start_d[i]>=max_d:
+            max_d = start_d[i]
+            i_max = i
+    min_d = start_d[0]
+    i_min = 0
+    for i in range(p_num):
+        if start_d[i]<=min_d:
+            min_d = start_d[i]
+            i_min = i
+    center1 = points[i_min]
+    center2 = points[i_max]
+
+    cluster1 = list()
+    cluster2 = list()
 
     while(True):
+        tmp_cluster1 = tmpCluster(cluster1)
+        tmp_cluster2 = tmpCluster(cluster2)
+
         center1_cosine_similarities = cosineSimilarity(center1, points)
         center2_cosine_similarities = cosineSimilarity(center2, points)
+        
         cluster1 = list()
         cluster2 = list()
         center_avg1 = [0,0,0,0]
         center_avg2 = [0,0,0,0]
+
         for i in range(p_num):
-            if center1_cosine_similarities[i]<=center2_cosine_similarities[i]:
+            if center1_cosine_similarities[i]>=center2_cosine_similarities[i]:
                 cluster1.append(points[i])
                 center_avg1[0]+=float(points[i][0])
                 center_avg1[1]+=float(points[i][1])   
@@ -79,7 +102,7 @@ def kMeans(points):
                 center_avg2[2]/=2
                 center_avg2[3]/=2
 
-        if center1==center_avg1 and center2==center_avg2:
+        if cluster1==tmp_cluster1 and cluster2==tmp_cluster2:
             break
         center1 = center_avg1
         center2 = center_avg2
@@ -103,6 +126,17 @@ def calculateS(cluster1, cluster2):
 
 points = readDataFile('dataset.csv')
 cluster1, cluster2 = kMeans(points)
+print(len(cluster1))
+print(len(cluster2))
 s = calculateS(cluster1, cluster2)
 print(s)
 
+with open('result3.csv', 'w',newline='') as write_file:
+    csv_writer = csv.writer( write_file )
+    for c in cluster1:
+        csv_writer.writerow(c)
+
+with open('result4.csv', 'w',newline='') as write_file:
+    csv_writer = csv.writer( write_file )
+    for c in cluster2:
+        csv_writer.writerow(c)   
--- euclideanDistance.py
+++ euclideanDistance.py
@@ -10,6 +10,13 @@ def readDataFile(file_name):
             points.append(row)
     return points
 
+def findStartDistance(points):
+    distances = list()
+    for point in points:
+        d = math.sqrt(float(point[0])**2+float(point[1])**2+float(point[2])**2+float(point[3])**2)
+        distances.append(d)
+    return distances
+
 def euclideanDistance(point, points):
     distances = list()
     for each_point in points:
@@ -21,23 +28,46 @@ def euclideanDistance(point, points):
         distances.append(distance)
     return distances
 
+def tmpCluster(cluster):
+    tmp = list()
+    for i in cluster:
+        tmp.append(i)
+    return tmp
+
 def kMeans(points):
     p_num = len(points)
 
-    i1 = random.randint(0, p_num-1)
-    i2 = random.randint(0, p_num-1)
-    while i1==i2:
-        i2 = random.randint(0, p_num-1)
-    center1 = points[i1]
-    center2 = points[i2]
+    start_d = findStartDistance(points)
+    max_d = 0
+    i_max = 0
+    for i in range(p_num):
+        if start_d[i]>=max_d:
+            max_d = start_d[i]
+            i_max = i
+    min_d = start_d[0]
+    i_min = 0
+    for i in range(p_num):
+        if start_d[i]<=min_d:
+            min_d = start_d[i]
+            i_min = i
+    center1 = points[i_min]
+    center2 = points[i_max]
+
+    cluster1 = list()
+    cluster2 = list()
 
     while(True):
+        tmp_cluster1 = tmpCluster(cluster1)
+        tmp_cluster2 = tmpCluster(cluster2)
+
         center1_euclidean_distances = euclideanDistance(center1, points)
         center2_euclidean_distances = euclideanDistance(center2, points)
+
         cluster1 = list()
         cluster2 = list()
         center_avg1 = [0,0,0,0]
         center_avg2 = [0,0,0,0]
+
         for i in range(p_num):
             if center1_euclidean_distances[i]<=center2_euclidean_distances[i]:
                 cluster1.append(points[i])
@@ -60,7 +90,7 @@ def kMeans(points):
                 center_avg2[2]/=2
                 center_avg2[3]/=2
 
-        if center1==center_avg1 and center2==center_avg2:
+        if cluster1==tmp_cluster1 and cluster2==tmp_cluster2:
             break
         center1 = center_avg1
         center2 = center_avg2
@@ -80,38 +110,21 @@ def calculateD(cluster1, cluster2):
             flag = 1
     return global_min
 
-# def separating(d, points):
-#     group1 = list()
-#     group2 = list()
-#     while len(points)!=0:
-#         point = points[0]
-#         distances = euclideanDistance(point, points)
-#         i = len(points)-1
-#         flag = 0
-#         while i>0:
-#             if distances[i]<=d:
-#                 flag = 1
-#                 group1.append(points[i])
-#                 points.pop(i)
-#             i-=1
-#         if flag:
-#             group1.append(points[0])
-#         else:
-#             group2.append(points[0])
-#         points.pop(0)
-#     return (group1, group2)
 
 
-# points = readDataFile('dataset.csv')
-# cluster1, cluster2 = kMeans(points)
-# print(len(cluster1))
-# # print(cluster1)
-# # print(cluster2)
-# d = calculateD(cluster1, cluster2)
-# # print(d)
+points = readDataFile('dataset.csv')
+cluster1, cluster2 = kMeans(points)
+print(len(cluster1))
+print(len(cluster2))
+d = calculateD(cluster1, cluster2)
+print(d)
 
-# with open('result.csv', 'w',newline='') as write_file:
-#     csv_writer = csv.writer( write_file )
-#     for c in cluster1:
-#         csv_writer.writerow(c)
-    
+with open('result1.csv', 'w',newline='') as write_file:
+    csv_writer = csv.writer( write_file )
+    for c in cluster1:
+        csv_writer.writerow(c)
+
+with open('result2.csv', 'w',newline='') as write_file:
+    csv_writer = csv.writer( write_file )
+    for c in cluster2:
+        csv_writer.writerow(c)   
